6728c0ee4ad0ab88d3ac2f4981d0f9ff6dc16b90
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-12-07 16:29:45 -0800

    uncompress

--- main.java
+++ main.java
@@ -1,5 +1,43 @@
 class Test {
+  // public static void main(String[] args) {
+  //   /*
+  //   The prints method simply print text on the console and does not add any new line. While println adds new line after print text on console.
+  //   */
+  //   System.out.println("Hello, World!");
+  //   System.out.print("1...");
+  //   System.out.println("2...");
+  //   System.out.println("3...");
+  //   /*
+  //   Hello, World!
+  //   1...2..
+  //   3...
+  //   */
+  // }
+
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    String compressed = "2c3b";
+    String nums = "0123456789";
+    String uncompressed = "";
+    String currN = "";
+
+    for (int i = 0; i < compressed.length(); i++) {
+      char curr = compressed.charAt(i);
+      String currentString = String.valueOf(curr);
+
+      if (!nums.contains(currentString)) {
+        int n = Integer.parseInt(currN);
+        currN = "";
+
+        while (n > 0) {
+          uncompressed += curr;
+          n -= 1;
+        }
+
+      } else {
+        currN += currentString;
+      }
+
+    }
+    System.out.println(uncompressed);
   }
 }
