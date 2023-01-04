43ad2660aab5157e10994b3f8b59ca43e4ae3635
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2023-01-04 11:59:20 -0800

    class working properly

--- test/src/App.java
+++ test/src/App.java
@@ -1,5 +1,32 @@
+class Customer {
+  String name= "defaultName";
+  String size = "S";
+
+  public static void main(String[] args) {
+  }
+  void setSize(String newSize) {
+    // The void keyword specifies that a method should not have a return value.
+    size = newSize;
+  }
+  String getSize() {
+    System.out.println(size);
+    return size;
+  }
+  public void getName() {
+    System.out.println(name);
+  }
+  public void updateName(String n) {
+    this.name = n;
+  }
+}
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+      Customer newCustomer = new Customer();
+      newCustomer.getName();
+      newCustomer.getSize();
+      newCustomer.updateName("Eddie");
+      newCustomer.getName();
+      System.out.println("END");
     }
 }
